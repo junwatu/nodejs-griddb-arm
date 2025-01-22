b4d650603618c0049608b7a7c3f0419c0419900d
Equan P <equan.p@gmail.com>
2025-01-22 20:27:03 +0700

    refactor main.js: update container name and row data for clarity

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
-import griddb from './db/griddb.js';
-import { getOrCreateContainer, insertData, queryData } from './db/griddbOperations.js';
+import griddb from "./db/griddb.js";
+import {
+  getOrCreateContainer,
+  insertData,
+  queryData,
+} from "./db/griddbOperations.js";
 
 (async () => {
-	try {
-		const containerName = "kolom";
-		const columnInfoList = [
-			["name", griddb.Type.STRING],
-			["status", griddb.Type.BOOL],
-			["count", griddb.Type.LONG],
-			["lob", griddb.Type.BLOB]
-		];
+  try {
+    const containerName = "species";
+    const columnInfoList = [
+      ["name", griddb.Type.STRING],
+      ["status", griddb.Type.BOOL],
+      ["count", griddb.Type.LONG],
+      ["lob", griddb.Type.BLOB],
+    ];
 
-		let container = await getOrCreateContainer(containerName, columnInfoList);
+    let container = await getOrCreateContainer(containerName, columnInfoList);
 
-		const rowData1 = ["manungso", true, 7, Buffer.from([65, 66, 67, 68, 69, 70, 71, 72, 73, 74])];
-		await insertData(container, rowData1);
+    const rowData1 = [
+      "human",
+      true,
+      7,
+      Buffer.from([65, 66, 67, 68, 69, 70, 71, 72, 73, 74]),
+    ];
+    await insertData(container, rowData1);
 
-		const rowData2 = ["haiwan", false, 5, Buffer.from([65, 66, 67, 68, 69, 70, 71, 72, 73, 74])];
-		await insertData(container, rowData2);
+    const rowData2 = [
+      "animal",
+      false,
+      5,
+      Buffer.from([65, 66, 67, 68, 69, 70, 71, 72, 73, 74]),
+    ];
+    await insertData(container, rowData2);
 
-		await queryData(container);
-
-	} catch (err) {
-		console.error("Error in main flow:", err.message);
-	}
+    await queryData(container);
+  } catch (err) {
+    console.error("Error in main flow:", err.message);
+  }
 })();
